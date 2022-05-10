68511dcc0a1a2548d71c6d5aa275236a0f96e468
kinamod <kina2k@gmail.com>
2022-05-10 12:57:01 +0100

    Update Content.js

--- src/components/Content.js
+++ src/components/Content.js
@@ -8,7 +8,9 @@ import contentData from "../utils/contentData";
 class Content extends Component {
   render() {
     return (
-      <div className="next-steps my-5">
+      <div className="next-steps my-5" style={{ 
+      backgroundImage: `url("https://wrap.org.uk/sites/default/files/styles/crop_image_style_4_1/public/2022-04/homepage-tree-sea-beach-banner-2000x647.jpg")` 
+    }}>
         <h2 className="my-5 text-center">What can I do next?</h2>
         <Row className="d-flex justify-content-between">
           {contentData.map((col, i) => (
