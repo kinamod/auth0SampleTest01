cae8e07bb0ff52ec588e186c39ec581c89cf8850
kinamod <kina2k@gmail.com>
2022-05-10 12:56:02 +0100

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,9 +3,7 @@ import React from "react";
 import logo from "../assets/wraplogo.svg";
 
 const Hero = () => (
-  <div className="text-center hero my-5" style={{ 
-      backgroundImage: `url("https://wrap.org.uk/sites/default/files/styles/crop_image_style_4_1/public/2022-04/homepage-tree-sea-beach-banner-2000x647.jpg")` 
-    }}>
+  <div className="text-center hero my-5" >
     <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" />
     <h1 className="mb-4">Wrap.org.uk</h1>
 
